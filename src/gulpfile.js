21af489a87df7108ee88b734dc264878be9255d3
Branden Dane <branden@danetheory.com>
2016-05-07 06:37:10 -0400

    Automation Tasks and minor boilerplate tweaks
    Defined automation tasks utilizing Gulp. Auto browser reload via
    Browser Sync on CSS/JS/HTML changes in dev. Autoprefixer for CSS. SCSS
    to CSS compilation.  CSSNANO for CSS and Uglify for JS in build. Added
    build script for optimized production build. Script for image
    optimization. Added basic SEO semantics into index.html, cuz why not?
    Setup a component driven SCSS folder hierarchy.

---
+++ src/gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp'),
+    gulpIf = require('gulp-if'),
+    rename = require('gulp-rename'),
+    cache = require('gulp-cache'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    bs = require('browser-sync').create();
+
+
+gulp.task('browser-sync', ['styles'], function() {
+    bs.init({
+        server: {
+            baseDir: "./client"
+        },
+        port: 8080
+
+    });
+});
+
+gulp.task('styles', function () {
+    return gulp.src('./client/public/styles/scss/**/*.scss')
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('./client/public/styles'))
+    .pipe(bs.reload({stream: true}))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('../build/public/styles'))
+    .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('scripts', function(){
+  gulp.src('./client/public/scripts/**/*.js')
+    .pipe(gulp.dest('./client/public/scripts'))
+    .pipe(bs.reload({stream: true, once: true}))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('../build/public/scripts'))
+    .pipe(bs.reload({stream: true, once: true}));
+});
+
+gulp.task('images', function(){
+  return gulp.src('./client/public/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin({
+    interlaced: true
+  })))
+  .pipe(gulp.dest('../build/public/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('./client/public/fonts/**/*')
+  .pipe(gulp.dest('../build/public/fonts'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('../build', {force: true});
+});
+
+gulp.task('build', function () {
+  runSequence('clean:dist',
+    ['styles', 'scripts', 'images', 'fonts']
+  )
+})
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("./client/public/styles/**/*.scss", ['styles']);
+    gulp.watch("./client/public/scripts/**/*.js", ['scripts']);
+    gulp.watch("./client/public/**/*.html").on('change', bs.reload);
+});
+
+gulp.task('default', function () {
+  runSequence(['styles','browser-sync', 'watch'])
+})
